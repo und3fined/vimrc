bc90b40a6cc9e5108c901429f8139e475c83ce9f
und3fined <cuonglee1011@gmail.com>
2020-02-09 22:11:52 +0700

    Update install_awesome_vimrc.sh

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -7,8 +7,8 @@ echo 'set runtimepath+=~/.config/vim/runtimes
 
 source ~/.config/vim/runtimes/vimrcs/basic.vim
 source ~/.config/vim/runtimes/vimrcs/filetypes.vim
-source ~/.config/vim/runtimes/plugins_config.vim
-source ~/.config/vim/runtimes/extended.vim
+source ~/.config/vim/runtimes/vimrcs/plugins_config.vim
+source ~/.config/vim/runtimes/vimrcs/extended.vim
 
 try
 source ~/.config/vim/runtimes/my_configs.vim
