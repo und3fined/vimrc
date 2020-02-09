25964a30d1012403a0957783f973e9136bb28222
und3fined <cuonglee1011@gmail.com>
2020-02-09 21:42:46 +0700

    Update install_awesome_vimrc.sh

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 set -e
 
-cd ~/.vim_runtime
+cd ~/.config/vim/runtimes
 
-echo 'set runtimepath+=~/.vim_runtime
+echo 'set runtimepath+=~/.config/vim/runtimes
 
-source ~/.vim_runtime/vimrcs/basic.vim
-source ~/.vim_runtime/vimrcs/filetypes.vim
-source ~/.vim_runtime/vimrcs/plugins_config.vim
-source ~/.vim_runtime/vimrcs/extended.vim
+source ~/.config/vim/runtimes/vimrcs/basic.vim
+source ~/.config/vim/runtimes/vimrcs/filetypes.vim
+source ~/.config/vim/runtimes/plugins_config.vim
+source ~/.config/vim/runtimes/extended.vim
 
 try
-source ~/.vim_runtime/my_configs.vim
+source ~/.config/vim/runtimes/my_configs.vim
 catch
-endtry' > ~/.vimrc
+endtry' > ~/.config/vim/vimrc
 
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
